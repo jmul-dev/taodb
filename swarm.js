9a365687e153bc07b074ac2b05b38e4a30833bfd
Neil Harlow <neil@y-designs.com>
2019-05-24 15:17:54 -0700

    configurable swarm options

--- swarm.js
+++ swarm.js
@@ -1,19 +1,19 @@
 const discovery = require("discovery-swarm");
 const swarmDefaults = require("dat-swarm-defaults");
 
-module.exports = (taodb) => {
+module.exports = (taodb, opts = {}) => {
 	const dbKey = taodb.db.key.toString("hex");
-	const swarm = discovery(
-		swarmDefaults({
+	const swarmOpts = Object.assign(
+		{
 			id: dbKey,
 			stream: (peer) => {
 				return taodb.replicate();
 			}
-		})
+		},
+		opts
 	);
-
+	const swarm = discovery(swarmDefaults(swarmOpts));
 	swarm.join(dbKey);
-
 	swarm.on("connection", taodb.onConnection.bind(taodb));
 	return swarm;
 };
